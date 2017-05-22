6ee9a41afee3d56ab202f324a326e4c86f946bb9
bkinney33 <kinneybrennan33@gmail.com>
2017-05-22 13:30:41 -0600

    fixed android start

--- MobileApp/MyApplication/app/src/main/java/com/bbmedia/bbmediaapp/MainActivity.java
+++ MobileApp/MyApplication/app/src/main/java/com/bbmedia/bbmediaapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.bbmedia.bbmediaapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void submit(){
-        Intent i = new Intent(this, FileList.class);
-        System.out.println(i.toString());
-        startActivity(i);
-
+    public void submit(View view){
+        try {
+            Intent i = new Intent(this, FileList.class);
+            
+            startActivity(i);
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
     }
 }
