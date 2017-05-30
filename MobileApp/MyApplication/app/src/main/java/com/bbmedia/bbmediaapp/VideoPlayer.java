3a0033f386370215011dde98c4694ce278fcf970
Brad Gustafson <greeninja13@gmail.com>
2017-05-30 16:30:11 -0600

    not broken anymore

--- MobileApp/MyApplication/app/src/main/java/com/bbmedia/bbmediaapp/VideoPlayer.java
+++ MobileApp/MyApplication/app/src/main/java/com/bbmedia/bbmediaapp/VideoPlayer.java
@@ -1,10 +1,14 @@
 package com.bbmedia.bbmediaapp;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
@@ -13,26 +17,88 @@ import java.io.IOException;
  * Created by Brad on 5/24/2017.
  */
 
-public class VideoPlayer extends AppCompatActivity {
+public class VideoPlayer extends Activity implements SurfaceHolder.Callback {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_player);
+        Uri vidUri = Uri.parse("http://10.10.26.252:7777/");
+//
+//        SurfaceView screen = new SurfaceView(this);
+////        screen.
+//
+//
+//        MediaPlayer player = new MediaPlayer();
+//        try {
+//            player.setDataSource(this, vidUri);
+//            player.setDisplay(screen.getHolder());
+//            player.prepare();
+//            player.start();
+//        } catch(IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+
+    private MediaPlayer mp;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-        Uri vidUri = Uri.parse("http://10.10.26.252:7777/");
 
-        SurfaceView screen = new SurfaceView(this);
-        screen.
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        mPreview = (SurfaceView)findViewById(R.id.Video);
+        holder = mPreview.getHolder();
+        holder.setFixedSize(800, 480);
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mp = new MediaPlayer();
 
 
-        MediaPlayer player = new MediaPlayer();
+    }
+    protected void onPause(){
+        super.onPause();
+        mp.release();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        mp.setDisplay(holder);
+        play();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+
+    }
+    void play(){
         try {
-            player.setDataSource(this, vidUri);
-            player.setDisplay(screen.getHolder());
-            player.prepare();
-            player.start();
-        } catch(IOException e){
+            mp.setDataSource(this, vidUri);
+
+            mp.prepare();
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        mp.start();
     }
+
 }
